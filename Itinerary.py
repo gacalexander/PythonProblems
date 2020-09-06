24b89e5005d17c73b85bfdb988b1b9252a6b2ad8
catherinealexander2020 <70829268+catherinealexander2020@users.noreply.github.com>
2020-09-05 23:38:50 -0400

    Create Itinerary.py

---
+++ Itinerary.py
@@ -0,0 +1,46 @@
+segments = [("JFK", "DEN"), ("LAX", "ORD"), ("DEN", "SFO"), ("LAS", "LAX"), ("ORD", "ATL"), ("ATL", "JFK"), ("SFO", "LAS")]
+start = "JFK"
+
+def get_route(path, origin):
+    orig_start = origin
+    itinerary_list = []
+    itinerary_list.append(origin)
+
+    while len(path) >= 1:
+        current = None
+        directions = {}
+
+        for item in path:
+
+            if item[0] == origin:
+                directions[item[0]] = item[1]
+
+        if len(directions) >= 1:
+            current = min(directions.values())
+            remove_tuple = (origin, current)
+
+        try:
+            path.remove(remove_tuple)
+            if current != orig_start:
+                itinerary_list.append(current)
+                origin = current
+        except:
+            return "not valid intinerary"
+
+    return itinerary_list
+
+
+print(get_route(segments, start))
+
+import random
+# Test pilot route
+ports = ["JFK", "DEN", "SFO", "LAS", "LAX", "ORD", "ATL"]
+starting_port = random.sample(ports, 5)
+for p in starting_port:
+     itinerary = get_route(segments, p)
+     assert itinerary[0] == p, "incorrect port of origin for the itinerary"
+     for i, port in enumerate(itinerary[:-1]):
+         dest = find_dest(segments, port)
+         print(dest)
+         assert dest == itinerary[i+1], "incorrect itinerary"
+print("\n(Passed.)")
