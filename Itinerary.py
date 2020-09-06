c5ff01557cbf0fe1e9537253cbf29268d1557669
Santhi S <SanthiSridharan@users.noreply.github.com>
2020-09-05 23:57:20 -0400

    Update Itinerary.py

--- Itinerary.py
+++ Itinerary.py
@@ -1,47 +1,64 @@
+
+
+
+'''
+Exercise 3 (3 points). Now write a function get_route(segs, origin) that traces the complete itinerary of the pilot, given her first port of origin. Assume that the destination for a flight is the same as the origin for the next flight. The itinerary is completed when he arrives back at the starting port of origin. For example, if the starting port is "JFK", your function should return the list: ["JFK", "DEN", "SFO", "LAS", "LAX", "ORD", "ATL"].
+'''
+
 segments = [("JFK", "DEN"), ("LAX", "ORD"), ("DEN", "SFO"), ("LAS", "LAX"), ("ORD", "ATL"), ("ATL", "JFK"), ("SFO", "LAS")]
 start = "JFK"
+
+import random
+def get_route(path, origin):
+
+    
+    startSeg = [i[0] for i in segments]
+    print("22" , startSeg)
+    endSeg = [i[1] for i in segments]
+    print("22" , endSeg)
+    startPath = origin
+    endPath = ""
+    
+    itinerary_list =[]
+    while endPath != origin :
+
+        endPath == ""
+        
+        try:
+            print("startPath", startPath)
+            print("endPath" ,endPath)
+            
+            startSegIndex = startSeg.index(startPath)
+            print("startSegIndex" , startSegIndex)
+            endPath = endSeg[startSegIndex]
+            print("endPath" , endPath)
+            itinerary_list.append(startPath)
+            startPath = endPath
+            print("27" , itinerary_list)
+        except:
+            return "No valid path"
+        
+       
+        
+    
+
+        
+
+    return itinerary_list
+
 
 def find_dest(segs, origin):
     assert type(origin) is str
     assert type(segs) is list
     dest = [item[1] for item in segs if item[0] == origin]
     return ' ' .join(dest)
-
-def get_route(path, origin):
-    orig_start = origin
-    itinerary_list = []
-    itinerary_list.append(origin)
-
-    while len(path) >= 1:
-        current = None
-        directions = {}
-
-        for item in path:
-
-            if item[0] == origin:
-                directions[item[0]] = item[1]
-
-        if len(directions) >= 1:
-            current = min(directions.values())
-            remove_tuple = (origin, current)
-
-        try:
-            path.remove(remove_tuple)
-            if current != orig_start:
-                itinerary_list.append(current)
-                origin = current
-        except:
-            return "not valid intinerary"
-
-    return itinerary_list
-
-
-print(get_route(segments, start))
-
+    
+    
 import random
 # Test pilot route
 ports = ["JFK", "DEN", "SFO", "LAS", "LAX", "ORD", "ATL"]
 starting_port = random.sample(ports, 5)
+print(starting_port)
 for p in starting_port:
      itinerary = get_route(segments, p)
      assert itinerary[0] == p, "incorrect port of origin for the itinerary"
@@ -50,3 +67,4 @@ for p in starting_port:
          print(dest)
          assert dest == itinerary[i+1], "incorrect itinerary"
 print("\n(Passed.)")
+
